74de0a5175ea357bcbad5ca8061b9814f7228c55
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-12-15 23:47:52 +0300

    deploy

--- myphotosproject/settings.py
+++ myphotosproject/settings.py
@@ -23,11 +23,7 @@ from decouple import config
 #...... 
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 cloudinary.config( 
   cloud_name = config('CLOUD_NAME'), 
