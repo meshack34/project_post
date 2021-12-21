c0769da059faa8613f4eacf1b4f10624e47e67e2
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-12-21 19:48:36 +0300

    procfile file

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn awward.wsgi --log-file -
--- awward/urls.py
+++ awward/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from django.contrib import admin
+from django.contrib.auth import views 
+from django_registration.backends.one_step.views import RegistrationView
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('awwardsapp.urls')),
+    url('accounts/register/', RegistrationView.as_view(success_url='/'),name='django_registration_register'),
+    url('accounts/', include('django_registration.backends.one_step.urls')),
+    url('accounts/', include('django.contrib.auth.urls')),
+    url('logout/', views.logout_then_login, name='logout'), 
 ]
